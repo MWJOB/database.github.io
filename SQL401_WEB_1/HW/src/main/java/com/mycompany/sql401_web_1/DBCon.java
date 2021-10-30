0914377b70e0057854011e723432506bd1e589dc
MWJOB <61102798+MWJOB@users.noreply.github.com>
2021-10-30 09:03:07 +0900

    Add files via upload

---
+++ SQL401_WEB_1/HW/src/main/java/com/mycompany/sql401_web_1/DBCon.java
@@ -0,0 +1,74 @@
+/**
+ * @프로그램:DBon.java DB연결
+ * @author:학번:2016214378 이름:박민우
+ * @날짜: 2021.10.09
+ * 질의401:
+ * sql-401:
+ */
+package com.mycompany.sql401_web_1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement; 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+ public class DBCon {
+ Connection conn=null;
+ Statement stmt=null;
+ PreparedStatement pstmt=null;
+ ResultSet rs=null;
+ String user="c##min", passwd="5425"; //dalma Oracle 서버 id, pw
+ String Drv= "oracle.jdbc.OracleDriver";
+ String Url= "jdbc:oracle:thin:@localhost:1521:XE";
+ //String Drv = "com.mysql.cj.jdbc.Driver";
+ //String Url = "jdbc:mysql://localhost:3306/univ";
+ //String sql="select * from student";
+ 
+ /**
+ * @param 없음. 반환값 Connection 객체
+ */
+ public Connection getCon(){
+ try {
+ Class.forName(Drv);
+ conn = DriverManager.getConnection(Url, user,passwd);
+ // stmt = conn.createStatement();
+ // rs = stmt.executeQuery(sql);
+ }catch(ClassNotFoundException ex){
+ System.out.println("CNF Err1:"+ex.getException());
+ }catch(SQLException ex){
+    System.out.println("SQL Err2:"+ex.getSQLState());
+ }
+ return conn;
+}
+ 
+ public PreparedStatement getPstmt(String sql){
+ try {
+ Class.forName(Drv);
+ conn = DriverManager.getConnection(Url,user,passwd);
+ pstmt = conn.prepareStatement(sql);
+ //rs = stmt.executeQuery(sql);
+ }catch(ClassNotFoundException ex){
+ System.out.println("Err1:"+ex.getException());
+ }catch(SQLException ex){
+ System.out.println("Err2:"+ex.getErrorCode());
+ }
+ return pstmt;
+ }
+ 
+ public ResultSet getRS(String sql){
+ try {
+ Class.forName(Drv);
+ conn = DriverManager.getConnection(Url,user,passwd);
+ stmt = conn.createStatement();
+ rs = stmt.executeQuery(sql);
+ }catch(ClassNotFoundException ex){
+ System.out.println("Err1: CNFE");
+ }catch(SQLException ex){
+ System.out.println("Err2:"+ex.getSQLState());
+ }
+ return rs;
+ }
+ }
+
---
+++ SQL401_WEB_1/mavenproject1/prac/prac/prac/SQL101/src/main/java/com/mycompany/sql101/StudentBean.java
@@ -0,0 +1,45 @@
+package com.mycompany.sql101;
+
+/**
+ * @프로그램:Student bean getter and setter
+ * @author:학번:2016214378 이름:박민우
+ * @날짜: 2021.10.09
+ */
+public class StudentBean {
+    String id;
+    String name;
+    String deptName;
+    int totCred;
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public int getTotCred() {
+        return totCred;
+    }
+
+    public void setTotCred(int totCred) {
+        this.totCred = totCred;
+    }
+}
